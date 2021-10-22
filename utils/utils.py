b9a5f2a4b3c624115e0fa9da5eb1a32287df028c
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-10-22 23:06:25 +0530

    photometric error bug fix

--- utils/utils.py
+++ utils/utils.py
@@ -3,10 +3,10 @@ from PIL import Image
 
 def photometric_error(img1: np.ndarray, img2: np.ndarray) -> float:
     assert img1.shape == img2.shape
-    img1 = img1[:, :, :3]
-    img2 = img2[:, :, :3]
-    error = (img1 - img2 + 0.0)**2
-    error = np.sum(error)/float(img1.shape[0]*img1.shape[1])
+    img1 = img1[:, :, :3].astype('float')
+    img2 = img2[:, :, :3].astype('float')
+    error = (img1 - img2 )**2
+    error = error.sum()/(img1.shape[0]*img1.shape[1])
     return error
 
 
